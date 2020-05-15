6507b2ded3a2a01b01d616b6d0c980572852f98d
angadtewari09 <despacito0099@gmail.com>
2020-05-15 23:43:37 +0530

    some changes done according to review

--- website/app.js
+++ website/app.js
@@ -1,76 +1,58 @@
 /* Universal Variables */
+const openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiBaseURL = "http://localhost:3000/";
+// Personal API Key for OpenWeatherMap API
+const openWeatherMapAPIKey = "appid=f776b152b7c00409616908635de88c9b";
+
 // HTML element to listen for click events
 const button = document.getElementById('generate');
+button.addEventListener("click", () => getDataFromOpenWeatherAPI(openWeatherMapAPIKey));
 
-// HTML elements to get the values
-const zip = document.getElementById('zip');
-const feelings = document.getElementById('feelings');
+/* Function to GET Web API Data*/
+async function getDataFromOpenWeatherAPI(apiKey) {
+    let zipCode = document.getElementById("zip").value;
+    fetch(`${openWeatherMapBaseURL}${zipCode},us&${apiKey}`).then((response) => {
+        return response.json();
+    }).then((result) => {
+        postProjectData(result.main.temp);
+    });
+}
 
-// HTML elements to update dynamically
-const date = document.getElementById('date');
-const temp = document.getElementById('temp');
-const content = document.getElementById('content');
-
-// OpenWeatherApi configuration
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=' ;
-const APIKey = '&APPID=f776b152b7c00409616908635de88c9b' ;
-
-// Create a new date instance dynamically with JS
-let din = new Date()
-let newDate =  din.getDate() + ' / ' + (din.getMonth() + 1)  + ' / ' + din.getFullYear()
-
-// Fetch Weather Data from OpenWeatherApi
-const fetchWeather = async (baseURL, zip, apiKey) => {
-  try {
-    const request = await fetch(`${baseURL}${zip},us&units=metric${apiKey}`);
-    const result = await request.json();
-    // destructuring of the result object
-    const {
-      main: {temp},
-    } = result
-    return temp
-  } catch (e) {
-    console.error(e);
-  }
+/* Function to GET Project Data */
+async function getProjectData() {
+    fetch(apiBaseURL + 'projectData').then((response) => {
+        return response.json();
+    }).then((result) => {
+        document.getElementById("date").innerHTML = result.date;
+        document.getElementById("temp").innerHTML = JSON.stringify(result.temp);
+        document.getElementById("content").innerHTML = result.content;
+        document.getElementById("zip").value = "";
+        document.getElementById("feelings").value = "";
+    });
 }
 
 // POST Request to store date, temp and user input
-const saveData = async (baseURL, data) => {
-  try {
-    await fetch(baseURL, {
-      method: 'POST',
-      mode: 'cors' ,
-      credentials: 'same-origin' ,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
+async function postProjectData(temp) {
+    // Create a new date instance dynamically with JS
+    let din = new Date();
+    let newDate =  din.getDate()+'/'+(din.getMonth()+1)+'/'+ din.getFullYear();
+    let content = document.getElementById("feelings").value;
+    let data = {
+        date: newDate,
+        temp: temp,
+        content: content,
+    };
+    let options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    };
+    fetch(apiBaseURL + 'projectData', options).then((response) => {
+        return response.json();
+    }).then((result) => {
+        getProjectData()
     });
-  } catch (e) {
-    console.error("error", e);
-  }
 }
-
-// Update UI dynamically
-const updateUI = async (temperature, newDate, feelings) => {
-  date.innerText = newDate
-  temp.innerText = `${temperature} deg`
-  content.innerText = feelings
-};
-
-// Event listener
-button.addEventListener('click', () => {
-  fetchWeather(baseURL, zip.value, APIKey)
-    .then(temp => {
-      return {date: newDate, temp, content: feelings.value}
-    })
-    .then(data => {
-      saveData('/api/projectdata', data)
-      return data
-    })
-    .then(({temp, date, content}) => updateUI(temp, date, content))
-    .catch(e => {
-      // There can be proper error handling with UI
-      console.error(e)
-    })
-});
